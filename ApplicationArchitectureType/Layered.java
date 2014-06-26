eb8807df1adc7a5d61681d4970afd9563f874b54
Subrata-cisco <saha.subrata@gmail.com>
2014-06-26 22:38:15 +0530

    arch changes

--- ApplicationArchitectureType/Layered.java
+++ ApplicationArchitectureType/Layered.java
@@ -1,5 +1,22 @@
 package ApplicationArchitectureType;
-
+/**
+ * Some time it is known as Tiered approach. 
+ * @author Subrata Saha.
+ *
+ */
 public class Layered {
 
+	/*
+	
+	Client Tier - Application clients [GUI,Applets,WAP phone etc]
+	
+	Presentation Tier - JSP, Servlets other UI elements [SSO,Session management,Content creation]
+	
+	Business Tier - EJB and other Business objects.[ Business logic,transaction ,data service]
+	
+	Integration tier - JMS , JDBC ,Connectors and Legacy [Resouce adapters ,rule engines etc.]
+	
+	Resource Tier - Databases , External system [data and External services]
+	
+	*/
 }
